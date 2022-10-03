fc086378a502fcb6d18084743aad61904917e482
jonphamm <jonpham09@gmail.com>
2022-10-03 13:56:34 -0400

    Add responsive contact form

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-// side menu for mobile devices
-var sideMenu = document.getElementById('#sidemenu');
-
+// responsive side menu open/close for mobile devices //
+let sideMenu = document.getElementById('side-menu');
 function openmenu() {
     sideMenu.style.right = '0'; // whenever right is 0, menu will be visible
 }
